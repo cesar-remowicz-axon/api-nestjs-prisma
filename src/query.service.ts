01be7ce773d5c77472e60fef791fc8cc262f3ccd
Cesar Remowicz Lukaski Junior <92342853+cesar-remowicz-axon@users.noreply.github.com>
2024-02-18 19:52:18 -0300

    Prisma fix

--- src/query.service.ts
+++ src/query.service.ts
@@ -4,11 +4,6 @@ interface IValues {
     badge: string;
 }
 
-interface IQueryResponse {
-    employee: string;
-    badge: string;
-}
-
 export class QueryExecutor {
 
     private prisma = new PrismaService();
@@ -16,22 +11,22 @@ export class QueryExecutor {
 
     async query(params: IValues) {
 
-        const respose = { employee: '' }
+        const response = { employee: '' };
 
         try {
             const x = await this.prisma.employees.findMany({ select: { badge: true, employee: true }, where: { badge: params.badge } });
-            console.log("x", x)
+            console.log("x", x);
 
-            respose.employee = x[0].employee;
+            response.employee = x[0].employee;
 
             // const y = await this.prisma.history_pointed.findMany({ select: { odfNumber: true }, where: { odfNumber: 2124340 } });
             // console.log("ye", y)
-            return [respose]
+            return [response]
         } catch (error) {
             console.log("Error", error)
         }
 
-        return [respose]
+        return [response]
     }
 
 }
